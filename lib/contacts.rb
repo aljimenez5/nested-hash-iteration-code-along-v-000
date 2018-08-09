2863159d6b7847aa759c8aade16d7b00fbce83fd
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-09 03:10:48 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, details_hash|
     if details_hash == :favorite_ice_cream_flavors
-        :favorite_ice_cream_flavors= :favorite_ice_cream_flavors.delete("strawberry")
+      :favorite_ice_cream_flavors.delete("strawberry")
     end
   end
 end
