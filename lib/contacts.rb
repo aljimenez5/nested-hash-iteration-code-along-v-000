3de7638262b27127d3e6bd365befcc6dd9beb8ad
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-09 02:53:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"].each do |person, info_hash|
+  contacts["Freddy Mercury"].each do |info_hash|
     binding.pry
     if person[1][info_hash] == :favorite_ice_cream_flavors
       info_hash.delete("strawberry")
